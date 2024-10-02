b603c5b90b2de120640ba41c887f5deea304de1e
streaTL <60083095+streaTL@users.noreply.github.com>
2024-10-02 21:49:56 +0900

    [Silver II] Title: N과 M (9), Time: 1104 ms, Memory: 65400 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    static boolean[] brr;
+    static int[] br;
+    static HashSet<String> hs = new HashSet<>();
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int[] arr = new int[x];
+        brr = new boolean[x];
+        br = new int[y];
+        for(int i=0;i<x;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        dfs(arr,0,y);
+
+    }
+
+    static void dfs(int[] arr, int a, int c){
+        if(a==c){
+            String s="";
+            for(int i=0;i<c;i++){
+                s+=br[i]+" ";
+            }
+            if(!hs.contains(s)){
+                hs.add(s);
+                for(int i=0;i<c;i++){
+                    System.out.print(br[i]+" ");
+                }
+                System.out.println();
+            }
+            return;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(!brr[i]){
+                brr[i]=true;
+                br[a]=arr[i];
+                dfs(arr,a+1,c);
+                brr[i]=false;
+            }
+        }
+    }
+
+}

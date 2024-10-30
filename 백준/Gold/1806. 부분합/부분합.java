8b2516da0ee30124a357c435c9accc078a80d604
streaTL <60083095+streaTL@users.noreply.github.com>
2024-10-30 15:06:14 +0900

    [Gold IV] Title: 부분합, Time: 468 ms, Memory: 26200 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,17 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
         ArrayList<Integer> ar = new ArrayList<>();
         int x=0;
         int z=1000000;
+        st=new StringTokenizer(br.readLine());
         for(int i=0;i<a;i++){
-            int y = sc.nextInt();
+            int y = Integer.parseInt(st.nextToken());
             x+=y;
             ar.add(y);
             if(x>=b){

f483663ec165dc791608584914bfd43bffbcad73
streaTL <60083095+streaTL@users.noreply.github.com>
2024-11-06 14:48:58 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        if(a%2==0)
+            System.out.println("CY");
+        else System.out.println("SK");
+    }
+}

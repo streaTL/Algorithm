d0e28251d2f8142b1aa388ebc052a1e05302bb73
streaTL <60083095+streaTL@users.noreply.github.com>
2024-12-17 16:13:44 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 648 ms, Memory: 65516 KB -BaekjoonHub

--- 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.java
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.java
@@ -1,36 +1,35 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] tree, arr;
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    static int[] tree;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        arr = new int[N + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[a+1];
+        int h = (int)Math.ceil(Math.log(a)/Math.log(2));
+        int size = (int)Math.pow(2,h+1);
+        tree = new int[size];
+        for(int i=1;i<=a;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
         }
-        tree = new int[N * 4];
-        init(1, N, 1);
-        int M = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        arr[0]=Integer.MAX_VALUE;
         StringBuilder sb = new StringBuilder();
-        while (M-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            if (a == 1) {
-                update(1, N, 1, b, c);
-            } else if (a == 2) {
-                sb.append(query(1, N, 1, b, c) + "\n");
+        init(arr,1,1,a);
+        for(int i=0;i<b;i++){
+            st=new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            if(x==1){
+                arr[y]=z;
+                update(arr,1,y,1,a);
+            }
+            else{
+                sb.append(pick(arr,1,y,z,1,a)+"\n");
             }
         }
         bw.write(sb.toString());
@@ -38,66 +37,55 @@ public class Main {
         bw.close();
         br.close();
     }
-    public static void init(int start, int end, int node) {
-        if (start == end) {
-            tree[node] = start;
-        } else {
-            int mid = (start + end) / 2;
-            init(start, mid, node * 2);
-            init(mid + 1, end, node * 2 + 1);
 
-            if (arr[tree[node * 2]] <= arr[tree[node * 2 + 1]]) {
-                tree[node] = tree[node * 2];
+    private static void init(int[] arr, int node, int start, int end){
+        if(start==end)
+            tree[node]=start;
+        else {
+            init(arr,node*2,start,(start+end)/2);
+            init(arr,node*2+1,(start+end)/2+1,end);
+            if (arr[tree[node*2]] <= arr[tree[node*2+1]]) {
+                tree[node] = tree[node*2];
             } else {
-                tree[node] = tree[node * 2 + 1];
+                tree[node] = tree[node*2+1];
             }
         }
     }
-    public static int query(int start, int end, int node, int left, int right) {
-        if (left > end || right < start) {
-            return -1;
-        }
 
-        if (left <= start && end <= right) {
+    private static void update(int[] arr, int node, int idx, int start, int end){
+        if(start>idx||end<idx)
+            return;
+        if(start==end){
+            tree[node]= idx;
+            return;
+        }
+        update(arr,node*2,idx,start,(start+end)/2);
+        update(arr,node*2+1,idx,(start+end)/2+1,end);
+        if (arr[tree[node*2]] <= arr[tree[node*2+1]]) {
+            tree[node] = tree[node*2];
+        }
+        else {
+            tree[node] = tree[node*2+1];
+        }
+    }
+    private static int pick(int[] arr,int node, int left, int right, int start, int end){
+        if(left<=start&&right>=end)
             return tree[node];
-        }
+        if(left>end||right<start)
+            return 0;
+        
+        int m1 = pick(arr,node*2,left,right,start,(start+end)/2);
+        int m2 = pick(arr,node*2+1,left,right,(start+end)/2+1,end);
 
-        int mid = (start + end) / 2;
-        int m1 = query(start, mid, node * 2, left, right);
-        int m2 = query(mid + 1, end, node * 2 + 1, left, right);
-
-        if (m1 == -1) {
+        if(m1==0){
             return m2;
-        } else if (m2 == -1) {
+        } else if (m2==0) {
             return m1;
-        } else {
-            if (arr[m1] <= arr[m2]) {
-                return m1;
-            } else {
+        }else {
+            if(arr[m1]>arr[m2]){
                 return m2;
             }
+            else return m1;
         }
     }
-
-    public static void update(int start, int end, int node, int idx, int val) {
-        if (idx < start || idx > end) {
-            return;
-        }
-        if (start == end) {
-            tree[node] = idx;
-            arr[idx] = val;
-            return;
-        }
-
-        int mid = (start + end) / 2;
-        update(start, mid, node * 2, idx, val);
-        update(mid + 1, end, node * 2 + 1, idx, val);
-
-        if (arr[tree[node * 2]] <= arr[tree[node * 2 + 1]]) {
-            tree[node] = tree[node * 2];
-        } else {
-            tree[node] = tree[node * 2 + 1];
-        }
-    }
-
 }

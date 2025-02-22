4f56682649d51d8f44309fd632ba2998ac730a87
streaTL <60083095+streaTL@users.noreply.github.com>
2025-02-22 15:46:21 +0900

    [Gold I] Title: 커피숍2, Time: 884 ms, Memory: 91812 KB -BaekjoonHub

---
+++ 백준/Gold/1275. 커피숍2/커피숍2.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long[] tree;
+    static int size;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        long[] arr = new long[a+1];
+        int c = (int)Math.ceil(Math.log(a)/Math.log(2));
+        size = (int)Math.pow(2,c+1);
+        tree = new long[size];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=a;i++){
+            arr[i]=Long.parseLong(st.nextToken());
+        }
+        StringBuilder sb = new StringBuilder();
+        init(arr,1,1,a);
+        for(int i=0;i<b;i++){
+            st=new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            long z = Long.parseLong(st.nextToken());
+            if(w<x)
+                sb.append(pick(1,1,a,w,x)+"\n");
+            else sb.append(pick(1,1,a,x,w)+"\n");
+            update(1,1,a,y,z-arr[y]);
+            arr[y]=z;
+        }
+        System.out.println(sb);
+    }
+    private static long init(long[] arr, int node, int start, int end){
+        if(start==end){
+            return tree[node]=arr[start];
+        }
+        return tree[node] = init(arr,node*2,start,(start+end)/2)+init(arr,node*2+1,(start+end)/2+1,end);
+    }
+    private static void update(int node, int start, int end, int idx, long m){
+        if(idx>end||idx<start)
+            return;
+        tree[node]+=m;
+        if(start==end)
+            return;
+        update(node * 2, start, (start + end) / 2, idx, m);
+        update(node * 2 + 1, (start + end) / 2 + 1, end, idx, m);
+    }
+    private static long pick(int node, int start, int end, int left, int right){
+        if(left>end||right<start){
+            return 0;
+        }
+        if(left <= start && end <= right){
+            return tree[node];
+        }
+        return pick(node*2, start, (start+end)/2, left, right)+pick(node*2+1, (start+end)/2+1, end, left, right);
+    }
+}

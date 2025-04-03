10970de93b4d77f19b6c52d82aef5b56ca41709d
streaTL <60083095+streaTL@users.noreply.github.com>
2025-04-03 17:52:42 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 96 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(2025);
+        System.out.println(04);
+        System.out.println(03);
+    }
+}

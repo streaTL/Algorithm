d5531e7b3503b5c833fcfe687cd4e194dc7a7682
streaTL <60083095+streaTL@users.noreply.github.com>
2025-04-06 17:35:54 +0900

    [Bronze II] Title: 정수의 개수, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        String[] srr = s.split(",");
+        System.out.println(srr.length);
+    }
+}

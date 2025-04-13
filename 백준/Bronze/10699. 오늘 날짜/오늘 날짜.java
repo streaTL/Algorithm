0a7a368deedf11a4ad544e5dc07e2ba9e0179ec8
streaTL <60083095+streaTL@users.noreply.github.com>
2025-04-13 18:16:59 +0900

    [Bronze V] Title: 오늘 날짜, Time: 100 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("2025-04-13");
+    }
+}

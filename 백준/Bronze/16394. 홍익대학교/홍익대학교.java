393c94f41acee61d06e1b22c284cd20f45b29341
streaTL <60083095+streaTL@users.noreply.github.com>
2025-05-11 23:17:19 +0900

    [Bronze V] Title: 홍익대학교, Time: 104 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        System.out.println(t-1946);
+    }
+}

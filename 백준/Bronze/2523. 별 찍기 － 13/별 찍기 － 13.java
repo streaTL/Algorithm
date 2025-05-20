3317e5d5152090155da96699bdf4ac7829f22937
streaTL <60083095+streaTL@users.noreply.github.com>
2025-05-20 22:14:52 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 216 ms, Memory: 16836 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=a-1;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

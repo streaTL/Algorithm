8579087a28bb0b61a7c0b4524fdc1699120f5f72
streaTL <60083095+streaTL@users.noreply.github.com>
2025-06-16 18:13:21 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 108 ms, Memory: 14540 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,15 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        double a = Double.parseDouble(br.readLine());
+        double b = Double.parseDouble(br.readLine());
+        System.out.printf("%f",2*a+Math.PI*2*b);
+    }
+}

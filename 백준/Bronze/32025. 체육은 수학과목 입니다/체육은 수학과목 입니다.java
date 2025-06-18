2e954b6e53e52e70ea3956c90edbd9bd638930df
streaTL <60083095+streaTL@users.noreply.github.com>
2025-06-18 14:04:40 +0900

    [Bronze IV] Title: 체육은 수학과목 입니다, Time: 120 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.java
@@ -0,0 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Math.min(a,b);
+        System.out.println(c*50);
+    }
+}

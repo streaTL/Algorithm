3fb68f83045b87c1901de417c199869a467e189a
streaTL <60083095+streaTL@users.noreply.github.com>
2025-08-01 19:27:54 +0900

    [Bronze V] Title: 제리와 톰, Time: 120 ms, Memory: 16048 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.println((m-n)+" "+m);
+    }
+
+}

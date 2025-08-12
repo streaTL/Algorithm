715400e1a87d81b9df449bd23a2882eec48f0c70
streaTL <60083095+streaTL@users.noreply.github.com>
2025-08-12 14:46:03 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 120 ms, Memory: 14548 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,21 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = a*b;
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            System.out.print((Integer.parseInt(st.nextToken())-c)+" ");
+        }
+
+    }
+
+}

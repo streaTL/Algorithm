a01523e94f59b370242c4c968976d0f155217b2c
streaTL <60083095+streaTL@users.noreply.github.com>
2025-08-13 16:00:38 +0900

    [Bronze IV] Title: 한글 2, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.read()-44031);
+
+    }
+
+}

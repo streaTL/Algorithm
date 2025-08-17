5137b203ad1d826e1136e80a1c9491ca128d96f4
streaTL <60083095+streaTL@users.noreply.github.com>
2025-08-17 11:08:30 +0900

    [Bronze IV] Title: 주사위, Time: 172 ms, Memory: 16528 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        for(int i=1;i<=t;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println("Case "+i+": "+(a+b));
+        }
+    }
+
+}

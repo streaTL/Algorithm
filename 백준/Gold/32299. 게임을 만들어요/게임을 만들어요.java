c084688be6bf6f4cb379897c1880b736f4bdb755
streaTL <60083095+streaTL@users.noreply.github.com>
2025-08-18 15:04:24 +0900

    [Gold III] Title: 게임을 만들어요, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Gold/32299. 게임을 만들어요/게임을 만들어요.java
+++ 백준/Gold/32299. 게임을 만들어요/게임을 만들어요.java
@@ -1,16 +1,9 @@
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.*;
-import java.util.StringTokenizer;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(br.readLine());
-        if(a==3)
-            System.out.println("Hobanwoo");
-        else System.out.println("Sangho");
+        System.out.println(a==3?"Hobanwoo":"Sangho");
     }
-
 }

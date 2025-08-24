32fa2574cf0b9a19b9a16f35491fe47b2be70306
streaTL <60083095+streaTL@users.noreply.github.com>
2025-08-24 14:32:00 +0900

    [Bronze IV] Title: 과자, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        System.out.println(c-a*b>=0?0:(c-a*b)*-1);
+    }
+}

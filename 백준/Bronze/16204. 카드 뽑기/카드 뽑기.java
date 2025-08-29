61af75ff0318407b8d9d810b9e2dc3fab39bc49f
streaTL <60083095+streaTL@users.noreply.github.com>
2025-08-29 17:02:08 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        if(b>=c){
+            System.out.println(c+a-b);
+        }
+        else{
+            System.out.println(b+a-c);
+        }
+    }
+}

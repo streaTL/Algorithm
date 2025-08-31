52bd8e730ebe40a48cc46b4e8d3408657b1e9ee2
streaTL <60083095+streaTL@users.noreply.github.com>
2025-08-31 15:42:20 +0900

    [Bronze V] Title: A+B, Time: 152 ms, Memory: 15704 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+        BigInteger c = a.add(b);
+        System.out.println(c);
+    }
+}

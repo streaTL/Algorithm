b15fd390d8b75801ae6a9ee9c03a41615db24394
streaTL <60083095+streaTL@users.noreply.github.com>
2025-09-01 19:03:29 +0900

    [Bronze IV] Title: Schronisko, Time: 112 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/8760. Schronisko/Schronisko.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for(int i=0;i<a;i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            System.out.println(x*y/2);
+        }
+    }
+}

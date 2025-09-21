04284870de055e96881fd05c2adfb70418411182
streaTL <60083095+streaTL@users.noreply.github.com>
2025-09-21 19:12:44 +0900

    [Bronze IV] Title: 베라의 패션, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(a*(a-1));
+    }
+}

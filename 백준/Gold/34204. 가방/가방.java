6489d96de6ae2ff51948957e5debc93e96a77f00
streaTL <60083095+streaTL@users.noreply.github.com>
2025-09-26 12:41:41 +0900

    [Gold IV] Title: 가방, Time: 492 ms, Memory: 76120 KB, Score: 69 point -BaekjoonHub

---
+++ 백준/Gold/34204. 가방/가방.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        int y=0;
+        int e = K-1;
+        int sum=0;
+        if(e<N-1){
+            for(int i=0;i<=e;i++){
+                sum+=arr[i];
+            }
+        }
+        else{
+            for(int i=0;i<N;i++){
+                sum+=arr[i];
+            }
+            for(int i=0;i<C;i++){
+                sb.append(sum).append("\n");
+            }
+            System.out.println(sb);
+            return;
+        }
+        int x = C;
+        while(true){
+            if(arr[y]<=x){
+                if(e==N){
+                    for(int i=0;i<=x;i++){
+                        sb.append(sum+"\n");
+                    }
+                    break;
+                }
+                if(y==0){
+                    for(int i=1;i<arr[y];i++){
+                        sb.append(sum+"\n");
+                    }
+                }
+                else {
+                    for(int i=0;i<arr[y];i++){
+                        sb.append(sum+"\n");
+                    }
+                }
+                x-=arr[y];
+                e++;
+                if(e<N){
+                    sum-=arr[y];
+                    y++;
+                    sum+=arr[e];
+                }
+            }
+            else{
+                for(int i=0;i<=x;i++){
+                    sb.append(sum+"\n");
+                }
+                break;
+            }
+        }
+        System.out.println(sb);
+    }
+}

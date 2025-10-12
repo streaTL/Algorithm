b192a27c831d5c54fc3952d7ec846534852e9830
streaTL <60083095+streaTL@users.noreply.github.com>
2025-10-12 16:54:15 +0900

    [Bronze V] Title: CAPS, Time: 256 ms, Memory: 21164 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s.toUpperCase());
+    }
+}
